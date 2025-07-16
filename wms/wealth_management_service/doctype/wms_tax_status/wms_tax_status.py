9cfc7268295affb079a74480e292ceb0d503c0d6
Piyush Sawhney <piyush.sawhney@knaps.in>
2025-07-16 07:44:51 +0000

    Creating WMS Family and members doctype and deleting risk profile for now

--- wms/wealth_management_service/doctype/wms_tax_status/wms_tax_status.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, KNAPS and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class WMSTaxStatus(Document):
-	pass
