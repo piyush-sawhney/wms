306a47133c6f15a213fccf16aa864e3ac6d4e9c4
Piyush Sawhney <piyushsawhney@outlook.com>
2025-07-26 14:57:55 +0530

    Basic Unit Testing setup

--- wms/wealth_management_service/doctype/wms_client/test_wms_client.py
+++ wms/wealth_management_service/doctype/wms_client/test_wms_client.py
@@ -1,9 +1,33 @@
 # Copyright (c) 2025, KNAPS and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
+
+def create_test_data():
+	frappe.set_user("Administrator")
+	doc = frappe.get_doc({
+ 		"doctype": "Contact",
+ 		"first_name": "Test Client 1" 
+		}).insert()
+	
+	doc = frappe.get_doc({
+ 		"doctype": "WMS Client",
+ 		"client_name": "Test Client 1",
+		"client_details": "Test Client 1"
+		}).insert()
+	
+
 
 
 class TestWMSClient(FrappeTestCase):
-	pass
+	@classmethod
+	def setUpClass(cls):
+		super().setUpClass()
+		create_test_data()
+	
+
+	def test_client_creation(self):
+		client = frappe.get_last_doc("WMS Client")
+		self.assertEqual(client.type, "Individual")
+		self.assertEqual(client.client_name, "Test Client 1")
