b98239f14aa17e3354336be6bb196fa0237d7cce
Piyush Sawhney <piyushsawhney@outlook.com>
2025-07-29 15:58:19 +0530

    Validations for WMS Bank doctype and customising list view settings for child table address and bank

--- wms/wealth_management_service/doctype/wms_bank/wms_bank.py
+++ wms/wealth_management_service/doctype/wms_bank/wms_bank.py
@@ -1,9 +1,45 @@
 # Copyright (c) 2025, KNAPS and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class WMSBank(Document):
-	pass
+	def before_save(self):
+		self.format_fields()
+	
+	def validate(self):
+		self.validate_micr()
+		self.validate_ifsc()
+		self.validate_bank_name()
+	
+	def validate_bank_name(self):
+		if not self.bank_name:
+			frappe.throw("Bank Name is required.")
+		if len(self.bank_name) < 3:
+			frappe.throw("Bank Name must be at least 3 characters long.")
+
+	def validate_ifsc(self):
+		if self.ifsc:
+			self.ifsc = self.ifsc.replace(" ", "")
+			if len(self.ifsc) != 11:
+				frappe.throw("IFSC code must be exactly 11 characters long.")
+			if not self.ifsc.isalnum():
+				frappe.throw("IFSC code must contain only alphanumeric characters.")
+
+	def format_fields(self):
+		self.bank_name = self.bank_name.strip().title()
+		if self.ifsc:
+			self.ifsc = self.ifsc.upper()
+		if self.branch:
+			self.branch = self.branch.strip().title()
+		
+		
+	def validate_micr(self):
+		if self.micr:
+			self.micr = self.micr.replace(" ", "")
+			if len(self.micr) != 9:
+				frappe.throw("MICR code must be exactly 9 digits long.")
+			if not self.micr.isdigit():
+				frappe.throw("MICR code must contain only digits.")
--- wms/wealth_management_service/doctype/wms_client/wms_client.js
+++ wms/wealth_management_service/doctype/wms_client/wms_client.js
@@ -1,8 +1,96 @@
 // Copyright (c) 2025, KNAPS and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("WMS Client", {
-// 	refresh(frm) {
+function custom_address_grid_view(frm) {
+    const child_table = "WMS Client Address";  // Child DocType
 
-// 	},
-// });
+    let address_grid_view = {};
+    address_grid_view[child_table] = [
+        {
+            "fieldname": "address_1",
+            "columns": 1
+        },
+        {
+            "fieldname": "address_2",
+            "columns": 2
+        },
+        {
+            "fieldname": "address_3",
+            "columns": 2
+        },
+        {
+            "fieldname": "district",
+            "columns": 1
+        },
+        {
+            "fieldname": "city",
+            "columns": 1
+        },
+        {
+            "fieldname": "country",
+            "columns": 1
+        },
+        {
+            "fieldname": "pincode",
+            "columns": 1
+        }
+    ];
+
+    frappe.model.user_settings.save(frm.doctype, "GridView", address_grid_view).then((r) => {
+        frappe.model.user_settings[frm.doctype] = r.message || r;
+
+        frappe.after_ajax(() => {
+            frm.fields_dict.addresses.grid.reset_grid();
+            frm.fields_dict.addresses.grid.refresh();
+        });
+
+        frm.refresh_fields("addresses");
+    });
+}
+
+function custom_bank_grid_view(frm) {
+    const child_table = "WMS Client Bank";  // Child DocType
+
+    let bank_grid_view = {};
+    bank_grid_view[child_table] = [
+        
+        {
+            "fieldname": "bank_name",
+            "columns": 2
+        },
+        {
+            "fieldname": "account_number",
+            "columns": 2
+        },
+        {
+            "fieldname": "type",
+            "columns": 2
+        },
+        {
+            "fieldname": "ifsc",
+            "columns": 2
+        },
+        {
+            "fieldname": "micr",
+            "columns": 2
+        }
+    ];
+
+    frappe.model.user_settings.save(frm.doctype, "GridView", bank_grid_view).then((r) => {
+        frappe.model.user_settings[frm.doctype] = r.message || r;
+
+        frappe.after_ajax(() => {
+            frm.fields_dict.addresses.grid.reset_grid();
+            frm.fields_dict.addresses.grid.refresh();
+        });
+
+        frm.refresh_fields("banks");
+    });
+}
+
+frappe.ui.form.on("WMS Client", {
+	refresh(frm) {
+        custom_address_grid_view(frm);
+        custom_bank_grid_view(frm);
+	}
+});
