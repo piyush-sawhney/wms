33bad13aba99e395713f09f0bb5a3d752ece7b83
Piyush Sawhney <piyushsawhney@outlook.com>
2025-07-30 16:30:20 +0530

    Creating Doctypes for Life Insurance

---
+++ wms/wms_insurance/doctype/wms_health_insurance_company/wms_health_insurance_company.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, KNAPS and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("WMS Health Insurance Company", {
+// 	refresh(frm) {
+
+// 	},
+// });
