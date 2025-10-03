23a20a45513a0f4e7300b9ba31feed571900b58d
Piyush Sawhney <piyush.sawhney@knaps.in>
2025-10-03 12:15:33 +0000

    chore: add commitlint and husky hooks

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    "subject-empty": [2, "never"],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",     // new feature
+        "fix",      // bug fix
+        "docs",     // documentation only changes
+        "style",    // formatting, code style changes
+        "refactor", // code change that neither fixes a bug nor adds a feature
+        "perf",     // performance improvements
+        "test",     // adding tests
+        "chore",    // tooling, configs, build process changes
+        "ci"        // CI/CD related changes
+      ],
+    ],
+  },
+};
