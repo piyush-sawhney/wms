8ae3646ae99b30826de021996838f6612e8ef3ea
Piyush Sawhney <piyush.sawhney@knaps.in>
2025-10-10 08:58:01 +0000

    refactor: adding name in filter for mf folio doctype

--- wms/wms_mutual_fund/doctype/wms_mf_folio/wms_mf_folio_list.js
+++ wms/wms_mutual_fund/doctype/wms_mf_folio/wms_mf_folio_list.js
@@ -1,5 +1,4 @@
 frappe.listview_settings["WMS MF Folio"] = {
 	add_fields: ["client", "folio_number", "amc"],
     hide_name_column: true, // hide the last column which shows the `name`
-    hide_name_filter: true,
 };
