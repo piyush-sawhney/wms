b40b23f119b34ab5a7bd6139ab5274a6ae887793
Piyush Sawhney <piyush.sawhney@knaps.in>
2025-11-03 11:45:09 +0000

    fix: handling guardian in mf folio Closes: #86

--- wms/wms_investment/doctype/wms_fixed_investment/wms_fixed_investment.py
+++ wms/wms_investment/doctype/wms_fixed_investment/wms_fixed_investment.py
@@ -2,6 +2,7 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe import _
 from frappe.model.document import Document
 from frappe.model.naming import make_autoname
 from frappe.utils import add_days, add_months, nowdate
@@ -77,9 +78,9 @@ class WMSFixedInvestment(Document):
 	def validate_minor_investment(self):
 		if self.client_classification == "Minor":
 			if self.guardian and self.guardian == self.client:
-				frappe.throw("Guardian cannot be the client.")
+				frappe.throw(_("Guardian cannot be the client."))
 			if self.holding_type != "Single":
-				frappe.throw("Holding Type must be 'Single' for Minor clients.")
+				frappe.throw(_("Holding Type must be 'Single' for Minor clients."))
 
 	def validate_is_active(self):
 		if self.status in ["Renewed", "Matured", "Pre-Matured", "Transmitted"]:
@@ -92,37 +93,37 @@ class WMSFixedInvestment(Document):
 			for nominee in self.nominees:
 				share_percentage += nominee.share_percent
 				if nominee.nominee_name == self.client:
-					frappe.throw("Client cannot be a nominee.")
+					frappe.throw(_("Client cannot be a nominee."))
 				if nominee.nominee_name in holders:
-					frappe.throw("Holder cannot be a nominee.")
+					frappe.throw(_("Holder cannot be a nominee."))
 				if self.guardian and nominee.nominee_name == self.guardian:
-					frappe.throw("Guardian cannot be a nominee.")
+					frappe.throw(_("Guardian cannot be a nominee."))
 				if nominee.dob:
 					if calculate_age(nominee.dob) < 18:
 						nominee.is_minor = 1
 						if not nominee.guardian_name or not nominee.guardian_relation:
-							frappe.throw("Guardian details are required for minor nominees.")
+							frappe.throw(_("Guardian details are required for minor nominees."))
 						if nominee.guardian_dob and calculate_age(nominee.guardian_dob) < 18:
-							frappe.throw("Guardian must be at least 18 years old.")
+							frappe.throw(_("Guardian must be at least 18 years old."))
 						if nominee.guardian_name == self.client:
-							frappe.throw("Client cannot be a guardian.")
+							frappe.throw(_("Client cannot be a guardian."))
 						if nominee.nominee_name == nominee.guardian_name:
-							frappe.throw("Nominee and Guardian cannot be the same person.")
+							frappe.throw(_("Nominee and Guardian cannot be the same person."))
 						if nominee.guardian_name in holders:
-							frappe.throw("Holder cannot be the guardian.")
+							frappe.throw(_("Holder cannot be the guardian."))
 
 			if share_percentage != 100:
-				frappe.throw("Total share percentage of nominees must be 100%.")
+				frappe.throw(_("Total share percentage of nominees must be 100%."))
 
 	def validate_holders(self):
 		if self.holders and len(self.holders) > 0:
 			if self.holding_type == "Single":
-				frappe.throw("Cannot add holders for Single Holding Type.")
+				frappe.throw(_("Cannot add holders for Single Holding Type."))
 			if len(self.holders) > 2:
-				frappe.throw("You can only add a maximum of 2 holders.")
+				frappe.throw(_("You can only add a maximum of 2 holders."))
 			for holder in self.holders:
 				if holder.holder == self.client:
-					frappe.throw("Client cannot be a holder.")
+					frappe.throw(_("Client cannot be a holder."))
 
 	def validate_dates(self):
 		now_date = nowdate()
@@ -130,11 +131,11 @@ class WMSFixedInvestment(Document):
 		print(type(now_date))
 		print(type(self.entry_date))
 		if self.entry_date and str(self.entry_date) > now_date:
-			frappe.throw("Entry Date cannot be in the future.")
+			frappe.throw(_("Entry Date cannot be in the future."))
 
 		if self.start_date:
 			if str(self.start_date) > now_date:
-				frappe.throw("Start Date cannot be in the future.")
+				frappe.throw(_("Start Date cannot be in the future."))
 			self.maturity_date = add_months(self.start_date, self.period)
 			self.maturity_date = add_days(self.maturity_date, -1)
 		else:
