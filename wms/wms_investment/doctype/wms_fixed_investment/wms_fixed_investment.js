b1dbb58c171e0871139570eaa5dd1820d4d99b3d
Piyush Sawhney <piyush.sawhney@knaps.in>
2025-11-04 17:52:06 +0000

    fix: ui issues with guardian investment when initaited from the client dashboard

--- wms/wms_investment/doctype/wms_fixed_investment/wms_fixed_investment.js
+++ wms/wms_investment/doctype/wms_fixed_investment/wms_fixed_investment.js
@@ -1,8 +1,20 @@
 // Copyright (c) 2025, KNAPS and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("WMS Fixed Investment", {
-// 	refresh(frm) {
+frappe.ui.form.on("WMS Fixed Investment", {
+	client(frm) {
+		frm.set_value("guardian", null);
 
-// 	},
-// });
+		if (frm.doc.client_classification === "Minor") {
+			frm.toggle_display("guardian", true);
+		} else {
+			frm.toggle_display("guardian", false);
+		}
+	},
+
+	guardian(frm) {
+		if (frm.doc.guardian === frm.doc.client) {
+			frm.set_value("guardian", null);
+		}
+	},
+});
